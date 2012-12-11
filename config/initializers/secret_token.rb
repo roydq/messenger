a476113ffbe6b4d32c0587856406c50c5a6e1bb1
Roy Quader <roybotnik@gmail.com>
2012-12-10 21:28:32 -0500

    initial rails app creation with mongoid instead of AR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messenger::Application.config.secret_token = 'bf9adca84f055e6e4748490cef35996c66c7ef4b5d71ec46e30491337cb40c572d83cb50d8e3539495f6358c054bb6975bd82cbce5f18fc9c162fb36788d6df7'
