2dc4a77184e1c0964943afc0eef3b3779241a273
Roy Quader <roybotnik@gmail.com>
2012-12-16 23:53:43 -0500

    user test tweak

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,7 @@ require "minitest_helper"
 
 class UserTest < ActiveSupport::TestCase
   test "shit works" do
-    user = Fabricate(:user)
-    user.username = 'shitter'
+    user = Fabricate(:user, :username => 'shitter')
     user.save
 
     assert_equal 'shitter', user.username
