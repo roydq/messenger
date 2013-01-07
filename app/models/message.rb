4118dc3961b3f0b446c24cf31c9a58e0af04657a
Roy Quader <roybotnik@gmail.com>
2013-01-06 23:09:34 -0500

    added more required fields to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -9,6 +9,10 @@ class Message
   field :location, type: String
 
   validates_presence_of :body
+  validates_presence_of :lat
+  validates_presence_of :lng
+  validates_presence_of :location
+  validates_presence_of :username
 
   belongs_to :user
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,12 +1,9 @@
 require "minitest_helper"
 
 class MessageTest < MiniTest::Rails::ActiveSupport::TestCase
-  test "shit works" do
+  test "fabricator works" do
     message = Fabricate(:message)
-    message.body = 'shitter'
-    message.save
-
-    assert_equal 'shitter', message.body
+    message.save!
   end
 
   test "message should require body" do
@@ -14,5 +11,26 @@ class MessageTest < MiniTest::Rails::ActiveSupport::TestCase
     message.body = nil
     assert !message.valid?, 'message should have been invalid without a body'
     assert message.errors.full_messages.include? "Body can't be blank"
+  end
+
+  test "message should require lat" do
+    message = Fabricate.build(:message)
+    message.lat = nil
+    assert !message.valid?, 'message should have been invalid without a lat'
+    assert message.errors.full_messages.include? "Lat can't be blank"
+  end
+
+  test "message should require lng" do
+    message = Fabricate.build(:message)
+    message.lng = nil
+    assert !message.valid?, 'message should have been invalid without a lng'
+    assert message.errors.full_messages.include? "Lng can't be blank"
+  end
+
+  test "message should require location" do
+    message = Fabricate.build(:message)
+    message.location = nil
+    assert !message.valid?, 'message should have been invalid without a location'
+    assert message.errors.full_messages.include? "Location can't be blank"
   end
 end
