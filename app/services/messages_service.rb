9d3feefb208495b0625dc7058a1c89bfe796215c
Roy Quader <roybotnik@gmail.com>
2013-02-17 22:58:54 -0500

    added service + tests

--- app/services/messages_service.rb
+++ app/services/messages_service.rb
@@ -1,2 +1,22 @@
 class MessagesService
+  attr_accessor :messages_data
+
+  def initialize(messages_data)
+    @messages_data = messages_data
+  end
+
+  def get_messages_near_coordinates(latitude, longitude, distance_in_miles=50, page=1)
+    distance_radians = miles_to_radians(distance_in_miles)
+    messages_data.within_circle([latitude, longitude], distance_radians).page(page)
+  end
+
+  def get_message_by_id(id)
+    messages_data.find(id)
+  end
+
+  private
+  # Converts miles to radians using the earth's equatorial radius
+  def miles_to_radians(miles)
+    miles/3963.192
+  end
 end
--- test/services/messages_service_test.rb
+++ test/services/messages_service_test.rb
@@ -1,4 +1,23 @@
 require "minitest_helper"
 
 class MessagesServiceTest < MiniTest::Rails::ActiveSupport::TestCase
+  before do
+    @data_stub = stub(:messages_data)
+    @messages_service = MessagesService.new(@data_stub)
+  end
+
+  test 'get_messages_near_coordinates should perform a location-based query' do
+    latitude = 10
+    longitude = 10
+    distance = 5
+    distance_radians = distance/3963.192
+    page = 2
+
+    circle_query_stub = stub(:query)
+    result_stub = stub(:result)
+    @data_stub.expects(:within_circle).with([latitude, longitude], distance_radians).returns(circle_query_stub)
+    circle_query_stub.expects(:page).with(2).returns(result_stub)
+
+    @messages_service.get_messages_near_coordinates(latitude, longitude, distance, page)
+  end
 end
