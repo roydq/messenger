dd391ed694157e87903d0e150a108efc7d406251
Roy Quader <roybotnik@gmail.com>
2013-02-25 12:43:57 -0500

    fixing geolocation

--- app/services/messages_service.rb
+++ app/services/messages_service.rb
@@ -8,7 +8,7 @@ class MessagesService
   def get_messages_near_coordinates(latitude, longitude, distance_in_miles=50, page=1)
     distance_in_miles = distance_in_miles || 50 # just in case nil is passed directly
     distance_radians = miles_to_radians(distance_in_miles)
-    messages_data.within_circle(coordinates: [[latitude, longitude], distance_radians]).page(page).entries
+    messages_data.within_spherical_circle(coordinates: [[longitude, latitude], distance_radians]).desc(:created_at).page(page).entries
   end
 
   def get_message_by_id(id)
@@ -26,6 +26,6 @@ class MessagesService
   private
   # Converts miles to radians using the earth's equatorial radius
   def miles_to_radians(miles)
-    miles/3963.192
+    (miles/3963.192) #*(Math::PI/180.0)
   end
 end
